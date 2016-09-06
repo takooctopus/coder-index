5d2bc4ff66ec1194d7d376ac244099c9bf4ddd2f
Liu <liuxindi@internal.baidu.com>
2016-09-06 19:00:43 +0800

    nearly done

--- dist/js/index.js
+++ dist/js/index.js
@@ -9,5 +9,21 @@ $(document).ready(function() {
     $('.flexslider').flexslider({
         animation: "slide"
     });
+
+    $('.flexslider2').flexslider({
+        animation: "slide"
+    });
+
+    $(".dynamic-body").delegate(".block", "mouseenter", function(){
+        setTimeout(function(){
+            $(this).addClass('block-hover');
+        }.apply(this), 5000)
+    })
+
+    $(".dynamic-body").delegate(".block", "mouseleave", function(){
+        $(this).removeClass('block-hover');
+    })
+
+
 });
 
